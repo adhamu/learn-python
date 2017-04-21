436a5f23dda0ea5bebbe520e41f5237a75fdb1b2
Amit Dhamu <amitdhamu@Amits-MBP.local>
2017-04-21 10:07:22 +0100

    Add some simple starters

---
+++ scripts/strings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+single_line = "amit"
+
+multi_line = """Hello world
+This is a multiline string and can
+go on
+for lots
+of lines"""
+
+print "Single Line: " + single_line + "\n"
+print "Multi Line: " + multi_line + "\n"
