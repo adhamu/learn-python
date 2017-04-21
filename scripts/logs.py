9e99806a7013b53096a40153ce83387af5b3e4a2
Amit Dhamu <amitdhamu@Amits-MBP.local>
2017-04-21 16:04:02 +0100

    Added coloured text logging with colourama

--- scripts/logs.py
+++ scripts/logs.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
 import logging
+from colorama import Fore, Back, Style
 
 logging.debug('Debugging information')
 logging.info('Informational message')
 logging.warning('Config file %s not found', 'server.conf')
 logging.error('Error occurred')
 logging.critical('Critical error -- shutting down')
+
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
