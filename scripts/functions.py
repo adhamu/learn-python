41988c0f05d461370bac950c0029131b4462bd18
Amit Dhamu <amitdhamu@Amits-MBP.local>
2017-04-21 14:03:12 +0100

    Simple functions

---
+++ scripts/functions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+def whats_my_name():
+    return "your name is amit"
+
+print whats_my_name()
+
+def my_name_is(name):
+    return "my name is " + name
+
+print my_name_is("amit")
+
+def default_name(name = None):
+    if name is None:
+        name = "Unknown"
+    return "my name is " + name
+
+print default_name();
+print default_name("Amit");
