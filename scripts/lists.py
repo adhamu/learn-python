290565369ad1cae62cf359631cff61178e563975
Amit Dhamu <dev@amitd.co>
2017-04-23 17:09:34 +0100

    Started lists

---
+++ scripts/lists.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# lists
+colours = ['red', 'blue', 'green']
+print(colours)
+print(colours[0])
+print(len(colours))
+
+# dicts
+dict = {}
+dict['a'] = 'apple'
+dict['b'] = 'ball'
+dict['c'] = 'cat'
+dict['d'] = 'dog'
+
+print(dict)
+print(dict['a'])
+print(len(dict))
