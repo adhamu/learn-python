796a9c2a5556c468ed08a2afddc537df8a4b502f
Amit Dhamu <DHAMUA@cpwplc.com>
2017-10-26 14:01:20 +0100

    Show how to write the contents of a file (similar to a bash heredoc)

---
+++ scripts/write-file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+with open("index.html", "w") as w:
+    w.write("""\
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Homepage</title>
+    </head>
+    <body>
+        <h1>Hello</h1>
+    </body>
+</html>
+""")
